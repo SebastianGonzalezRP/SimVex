edcb551d0e0d302e127b935f6ff2fb6c4c12ee62
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2023-05-25 20:05:47 -0400

    Node Factory

---
+++ factory/node_factory.py
@@ -0,0 +1,22 @@
+from models.nodes.node import Node  
+from models.nodes.start import Start
+from models.nodes.street import Street
+from models.nodes.stop import Stop
+from models.nodes.intersection import Intersection
+from models.nodes.end import End
+
+def node_generator(node):
+    new_node = Node()
+    node_type = list(node)[0]
+    print(node_type)
+    if node_type == 'Street':
+        new_node = Street(length=node[node_type]["length"],
+                          tracks=node[node_type]["tracks"])
+    elif node_type == 'Stop':
+        new_node = Stop(id=node[node_type]["id"],
+                        n_platform=node[node_type]["berths"])
+    elif node_type == 'Intersection':
+        new_node = Intersection(cicle_duration=node[node_type]["cicle"],
+                                effective_green=node[node_type]["green"])
+    return new_node
+
