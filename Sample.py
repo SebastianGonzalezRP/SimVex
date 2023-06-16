722b3667cdebaa8876225004fae72e79bbab5063
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2023-06-16 10:13:22 -0400

    Working Sample

--- Sample.py
+++ Sample.py
@@ -1,6 +1,15 @@
 import json
 from numpy import random
 from factory.file_generator.passenger_dispatcher import Passenger_Dispatcher
+from factory.file_generator.bus_dispatcher import Bus_Dispatcher
+import json
+from factory.file_generator.file_generator import FileGenerator
+from controllers.sim.sim_controller import Sim_Controller
+from factory.node_factory import *
+from models.transit_network import TransitNetwork
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 
@@ -22,15 +31,18 @@ FILE = {
         "101":{
             "stops":{"p-103":{"passenger_rate":{"Exp":{"rate":40}}},
                      "p-104":{"passenger_rate":{"Exp":{"rate":50}}}},
-            "bus_rate":{"Exp":{"rate":720}}},
+            "bus_rate":{"Exp":{"rate":720}},
+            "initial_occupancy":{"Fixed":{"rate":20}}},
         "102":{
             "stops":{"p-103":{"passenger_rate":{"Exp":{"rate":30}}}},
-            "bus_rate":{"Fixed":{"rate":600}}},
+            "bus_rate":{"Fixed":{"rate":600}},
+            "initial_occupancy":{"Normal":{"mu":20,"stdv":3}}},
         "103":{
             "stops":{"p-104":{"passenger_rate":{"Exp":{"rate":30}}}},
-            "bus_rate":{"Uniform":{"a":540,"b":720}}}
+            "bus_rate":{"Uniform":{"a":540,"b":720}},
+            "initial_occupancy":{"Uniform":{"a":15,"b":25}}}
     },  
-    "Buses":{"top_speed": 14, "acc":10,"desc":4},
+    "Buses":{"top_speed": 14, "acc":1,"desc":1},
     "Passenger":{
         "boarding_time_dist":{"Normal":{"mu":3,"stdv":1}},
         "alighting_time_dist":{"Uniform":{"a":2,"b":4}}
@@ -42,19 +54,45 @@ FILE = {
 #with open("generator.json", "w") as outfile:
 #    json.dump(FILE, outfile)
 
-# ID, origin, destiny, route, t_in, boarding_t, alighting_t
+#PASSENGER:  [t_in, ID, origin, destiny, route,  boarding_t, alighting_t]
+#BUS: [t_in, ID, route, door_n]
 
-PD = Passenger_Dispatcher(FILE)
-PD.generate_dispatcher_file()
+#PD = Passenger_Dispatcher(FILE)
+#PD.generate_dispatcher_file("TEST_SERIAL")
+
+#BD = Bus_Dispatcher(FILE)
+#BD.generate_dispatcher_file("TEST_SERIAL")
+
+#FG = FileGenerator(FILE)
+
+#json_data = json.dumps(FILE, indent=4)
+## Write JSON string to a file
+#with open("generator.json", "w") as file:
+#    file.write(json_data)
+
+generator_path = 'files\generator.json' 
+passenger_file_path  = r'files\2023_5_30_1253\passenger_dispatcher.csv'
+bus_file_path = r'files\2023_5_30_1253\bus_dispatcher.csv'
 
 
+SC = Sim_Controller(generator_path=generator_path,
+                    passenger_dispatch_path= passenger_file_path,
+                    bus_dispatch_path = bus_file_path)
+
+SC.debug()
+
+results = SC.get_results()
+
+sns.boxplot(data=results)
+plt.show()
+
+for line in results:
+    plt.plot(line)
+plt.show()
+
+df = pd.DataFrame(results)
+
+sns.lineplot(data=df.T)
+plt.show()
 
 
-#print(f"Exponencial: {random.exponential(720)}")
-#print(f"Uniforme: {random.uniform(2,4)}")
-#print(f"Normal: {round(random.normal(3,1))}")
-
-
-
-
-dict = {"Route": None, "Stop": None, }
