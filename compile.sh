89e2f3eea69d04f7be6d510a20b9e90a48619e7a
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2023-08-04 19:54:38 -0400

    - fix compiler

--- compile.sh
+++ compile.sh
@@ -2,13 +2,11 @@
 
 if [ ! -d ".venv" ]; then
     python -m venv .venv
-    source .venv/bin/activate
-    python.exe -m pip install --upgrade pip
-    pip install -r requirements.txt
-    deactivate
-fi
+fi  
 
-source .venv/bin/activate
+source .venv/Scripts/activate
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
 pyinstaller --onefile --name SimVex SimVex.py --distpath ./ --noconsole --clean
 deactivate
 
