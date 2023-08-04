b0e2cb7dcdfe079b0b1de40cedf4f2181e8a6781
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2023-08-04 19:23:30 -0400

    Mod Compiler

--- compile.sh
+++ compile.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ ! -d ".venv" ]; then
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+    deactivate
+fi
 
-
-# Activate the virtual environment
-source .venv/Scripts/activate
-
-pyinstaller --onefile --name SimVex SimVex.py --distpath ./ --noconsole
-
+source .venv/bin/activate
+pyinstaller --onefile --name SimVex SimVex.py --distpath ./ --noconsole --clean
 deactivate
+
+rm SimVex.spec
+rm -rf build
