7844f6e56f208214e7636461169f437e6cdac5a4
SebastianGonzalezRP <slgonzalez@miuandes.cl>
2023-11-20 01:31:51 -0300

    License Notice

--- SimVex.py
+++ SimVex.py
@@ -1,3 +1,22 @@
+##########################################################################
+"""
+This file is part of SimVex.
+
+SimVex is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or any later version.
+
+SimVex is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with SimVex.
+ If not, see <https://www.gnu.org/licenses/>.
+"""
+##########################################################################
+
+
+
 from controllers.view.view_controller import ViewController
 from controllers.sim.sim_controller import SimController
 from GUI.main_view import MainView
